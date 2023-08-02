a7fff5254c95f9c20eed17369b451f8614b80fe6
luuhuythanh0111 <hongnhungle491@gmail.com>
2023-08-02 22:50:45 +0700

    + Add Player

---
+++ Assets/_Game/Scripts/Player.cs
@@ -0,0 +1,59 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    [SerializeField] private Rigidbody2D rb;
+    [SerializeField] private LayerMask groundLayerMask;
+    private bool isGrounded;
+    private bool isJumping;
+    private bool isAttack;
+    private float horizontal;
+    private float vertical; 
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        horizontal = Input.GetAxisRaw("Horizontal");
+        vertical = Input.GetAxisRaw("Vertical");
+        //Debug.Log(CheckGrounded());
+
+    }
+    private bool CheckGrounded()
+    {
+        Debug.DrawLine(transform.position, transform.position + Vector3.down * 1.1f, Color.red);
+        RaycastHit2D hit = Physics2D.Raycast(transform.position, Vector2.down, 1.1f, groundLayerMask);
+        
+        //if (hit.collider != null)                    //va chạm với collider
+        //{
+        //    return true;
+        //}
+        //else
+        //{
+        //    return false;
+        //}
+
+        return hit.collider != null;
+    }
+    
+    private void Attack()
+    {
+
+    }
+    private void Throw()
+    {
+
+    }
+
+    private void Jump()
+    {
+
+    }
+
+}
